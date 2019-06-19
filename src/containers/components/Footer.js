0a8345c42463437f3ea2f6d9d25d367ed4491d8d
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2019-06-18 18:05:01 -0700

    premature happiness there gav el son

--- src/containers/components/Footer.js
+++ src/containers/components/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Footer () {
     return (
-        <p className = 'Footer'>All rights reserved Gavin Callander 2019</p>
+        <div className = 'Footer'>
+            <p>All rights reserved Gavin Callander 2019</p>
+        </div>
     )
 }
